49693a626104668096f6b446d53e9b5c0c545d32
whythat <whythat@protonmail.com>
2017-07-23 03:58:36 +0300

    'Condense' files from blocks in IPFS

---
+++ core/condense.go
@@ -0,0 +1,39 @@
+package core
+
+import (
+	"bytes"
+	"fmt"
+
+	base58 "github.com/jbenet/go-base58"
+)
+
+// Gradually fetch raw blocks from IPFS network and
+// decrypt them into a file
+func Condense(passphrase, headId string) (string, error) {
+	fmtstr := "Condensing file '%s' with passphrase '%s'\n"
+	fmt.Printf(fmtstr, headId, passphrase)
+
+	ipfsContext, err := NewIpfsContext()
+	if err != nil {
+		return "", err
+	}
+
+	var blocks [][]byte
+	nullBlockId, nextBlockId := make([]byte, IdSize), base58.Decode(headId)
+	for !bytes.Equal(nextBlockId, nullBlockId) {
+		fmt.Printf("Retrieving %s\n", base58.Encode(nextBlockId))
+		block, err := ipfsContext.GetBlock(nextBlockId)
+		if err != nil {
+			fstr := "[condense]: failed to retrieve a block: %s"
+			return "", fmt.Errorf(fstr, err)
+		}
+		var rawBlock []byte
+		nextBlockId, rawBlock = block[:IdSize], block[IdSize:]
+		blocks = append(blocks, rawBlock)
+	}
+	// Wait what? Really? It's 2017!
+	for i, j := 0, len(blocks)-1; i < j; i, j = i+1, j-1 {
+		blocks[i], blocks[j] = blocks[j], blocks[i]
+	}
+	return string(bytes.Join(blocks, []byte{})), nil
+}
