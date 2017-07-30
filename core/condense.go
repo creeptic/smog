66b8e4ff4ef7a02391117de85ed77408155f06b9
whythat <whythat@protonmail.com>
2017-07-30 13:07:22 +0300

    Data encryption draft

--- core/condense.go
+++ core/condense.go
@@ -4,28 +4,49 @@ import (
 	"bytes"
 	"fmt"
 
+	"github.com/creeptic/smog/pb"
+	"github.com/gogo/protobuf/proto"
 	base58 "github.com/jbenet/go-base58"
 )
 
 // Gradually fetch raw blocks from IPFS network and
 // decrypt them into a file
 func Condense(passphrase, headId string) (string, error) {
+	// Initialize IPFS connection
 	ipfsContext, err := NewIpfsContext()
 	if err != nil {
 		return "", err
 	}
 
+	// Get header from IPFS
+	headerBytes, err := ipfsContext.GetBlock(base58.Decode(headId))
+	if err != nil {
+		return "", err
+	}
+	var header pb.Header
+	if err := proto.Unmarshal(headerBytes, &header); err != nil {
+		return "", err
+	}
+
+	// Expand passphrase
+	cipher, err := NewSmogCipher(base58.Decode(passphrase), header.Salt, header.Nonce)
+	if err != nil {
+		return "", err
+	}
+
+	//
 	var blocks [][]byte
-	nullBlockId, nextBlockId := make([]byte, IdSize), base58.Decode(headId)
+	nullBlockId, nextBlockId := make([]byte, IdSize), header.Head
 	for !bytes.Equal(nextBlockId, nullBlockId) {
 		fmt.Printf("Retrieving %s\n", base58.Encode(nextBlockId))
-		block, err := ipfsContext.GetBlock(nextBlockId)
+		securedblock, err := ipfsContext.GetBlock(nextBlockId)
 		if err != nil {
 			fstr := "[condense]: failed to retrieve a block: %s"
 			return "", fmt.Errorf(fstr, err)
 		}
+		markedBlock := cipher.Decrypt(securedblock)
 		var rawBlock []byte
-		nextBlockId, rawBlock = block[:IdSize], block[IdSize:]
+		nextBlockId, rawBlock = markedBlock[:IdSize], markedBlock[IdSize:]
 		blocks = append(blocks, rawBlock)
 	}
 	// Wait what? Really? It's 2017!
--- core/vaporize.go
+++ core/vaporize.go
@@ -1,37 +1,90 @@
 package core
 
 import (
+	"crypto/rand"
 	"fmt"
+	"io/ioutil"
 
+	"github.com/creeptic/smog/pb"
+	"github.com/gogo/protobuf/proto"
 	base58 "github.com/jbenet/go-base58"
 )
 
 var (
 	BlockSize = 32
 	IdSize    = 34
+	KeySize   = 32
+	NonceSize = 16
+	SaltSize  = 32
 )
 
 // Read file, break it into blocks and publish them into IPFS network
 // gradually encrypting each one with viewKey
 func Vaporize(passphrase, filename string) (string, error) {
+	// Initialize IPFS connection
 	ipfsContext, err := NewIpfsContext()
 	if err != nil {
 		return "", err
 	}
-
-	fileBlocks, err := getFileBlocks(filename)
+	// Read raw data
+	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return "", err
 	}
+
+	// Break file data into blocks
+	fileBlocks := getBlocks(data, BlockSize)
+
+	// Generate required randomness
+	salt, nonce, err := generateRandomness()
+	if err != nil {
+		return "", err
+	}
+
+	// Expand passphrase into a suitable encryption key
+	cipher, err := NewSmogCipher(base58.Decode(passphrase), salt, nonce)
+	if err != nil {
+		return "", err
+	}
+
+	// Store blocks in IPFS chaining them by IDs and encrypting
 	nextBlockId := make([]byte, IdSize)
 	for _, rawBlock := range fileBlocks {
 		markedBlock := append(nextBlockId, rawBlock...)
-		nextBlockId, err = ipfsContext.PutBlock(markedBlock)
-		fmt.Printf("Storing %s\n\n", base58.Encode(nextBlockId))
+		securedBlock := cipher.Encrypt(markedBlock)
+		nextBlockId, err = ipfsContext.PutBlock(securedBlock)
 		if err != nil {
 			fstr := "[vaporize]: failed to put a new block: %s"
 			return "", fmt.Errorf(fstr, err)
 		}
+		fmt.Printf("Storing %s\n\n", base58.Encode(nextBlockId))
 	}
-	return base58.Encode(nextBlockId), nil
+
+	// Store header in IPFS, return its ID as a link to the data
+	header := pb.Header{Salt: salt, Nonce: nonce, Head: nextBlockId}
+	headerBytes, err := proto.Marshal(&header)
+	if err != nil {
+		return "", err
+	}
+	headerID, err := ipfsContext.PutBlock(headerBytes)
+	if err != nil {
+		return "", err
+	}
+	return base58.Encode(headerID), nil
+}
+
+func generateRandomness() ([]byte, []byte, error) {
+	buf := make([]byte, SaltSize+NonceSize)
+	if _, err := rand.Read(buf); err != nil {
+		return nil, nil, err
+	}
+	return buf[:SaltSize], buf[SaltSize:], nil
+}
+
+func getBlocks(data []byte, blocksize int) [][]byte {
+	var res [][]byte
+	for i := 0; i < len(data); i += blocksize {
+		res = append(res, data[i:i+blocksize])
+	}
+	return res
 }
