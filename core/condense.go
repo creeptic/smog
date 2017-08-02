df8090cf26526b28d77078b91bd4ec9912f6c42a
whythat <whythat@protonmail.com>
2017-08-02 04:03:03 +0300

    Move component condensing to sep. functions

--- core/condense.go
+++ core/condense.go
@@ -12,58 +12,79 @@ import (
 
 // Gradually fetch raw blocks from IPFS network and
 // decrypt them into a file
-func Condense(passphrase, headId string) (string, error) {
+func Condense(passphrase, head string) (string, error) {
 	// Initialize IPFS connection
-	ipfsContext, err := NewIpfsContext()
+	ipfs, err := NewIpfsContext()
 	if err != nil {
 		return "", err
 	}
 
 	// Get header from IPFS
-	headerBytes, err := ipfsContext.GetBlock(base58.Decode(headId))
+	header, err := condenseHeader(ipfs, base58.Decode(head))
 	if err != nil {
 		return "", err
+	}
+
+	// Restore keys
+	fkey, tkey := ExpandPassphrase([]byte(passphrase), header.Salt)
+
+	// Get table from IPFS
+	table, err := condenseTable(ipfs, tkey, header)
+	if err != nil {
+		return "", err
+	}
+
+	// Fetch and decrypt blocks
+	data, err := condenseData(ipfs, fkey, table)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+func condenseHeader(ipfs IpfsContext, headID []byte) (*pb.Header, error) {
+	headerBytes, err := ipfs.GetBlock(headID)
+	if err != nil {
+		return nil, err
 	}
 	var header pb.Header
 	if err := proto.Unmarshal(headerBytes, &header); err != nil {
-		return "", err
+		return nil, err
 	}
-	salt, tnonce, tableID := header.Salt, header.Nonce, header.TableID
+	return &header, nil
+}
 
-	// Restore keys
-	fkey, tkey := ExpandPassphrase([]byte(passphrase), salt)
-
-	// Fetch table from IPFS and decrypt its data
-	tbytes, err := ipfsContext.GetBlock(tableID)
+// Fetch table from IPFS and decrypt its data
+func condenseTable(ipfs IpfsContext, tkey []byte, header *pb.Header) (*pb.BlockTable, error) {
+	tbytes, err := ipfs.GetBlock(header.TableID)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	tcipher, err := NewSmogCipher(tkey, tnonce)
+	tcipher, err := NewSmogCipher(tkey, header.Nonce)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	var table pb.BlockTable
 	if err := proto.Unmarshal(tcipher.Decrypt(tbytes), &table); err != nil {
-		return "", err
+		return nil, err
 	}
-	fnonce, blockIDs := table.Nonce, table.Blocks
+	return &table, nil
+}
 
-	// Fetch blocks one by one from IPFS, encrypting each one
-	// and getting next block ID from it
+// Fetch blocks from IPFS and return decryption of their concatenation
+func condenseData(ipfs IpfsContext, fkey []byte, table *pb.BlockTable) ([]byte, error) {
 	blocks := make([][]byte, 0)
-	for _, blockID := range blockIDs {
-		block, err := ipfsContext.GetBlock(blockID)
+	for _, blockID := range table.Blocks {
+		block, err := ipfs.GetBlock(blockID)
 		if err != nil {
 			fstr := "[condense]: failed to retrieve a block: %s"
-			return "", errors.New(fmt.Sprintf(fstr, err))
+			return nil, errors.New(fmt.Sprintf(fstr, err))
 		}
 		blocks = append(blocks, block)
 	}
-
-	// Concatenate blocks and return decrypted result
-	fcipher, err := NewSmogCipher(fkey, fnonce)
+	fcipher, err := NewSmogCipher(fkey, table.Nonce)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return string(fcipher.Decrypt(bytes.Join(blocks, []byte{}))), nil
+	return fcipher.Decrypt(bytes.Join(blocks, []byte{})), nil
 }
